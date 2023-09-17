1bcdf0e0e9abf58e82bd91aedf4ec03c998fd40d
Juan Cruz Gaggioli <jcgaggioli@gmail.com>
2023-09-17 20:45:20 +0200

    style: navbar backround transition added

--- src/App.js
+++ src/App.js
@@ -1,65 +1,62 @@
-import "./styles/App.scss";
-import "./styles/_styles.scss";
-import { NavBar } from "./component/NavBar";
-import Home from "./component/Home";
-import AboutMe from "./component/AboutMe";
-import Contact from "./component/Contact";
-import Projects from "./component/Projects";
-import Frelance from "./component/Frelance";
-import { useEffect, useState } from "react";
+import './styles/App.scss';
+import './styles/_styles.scss';
+import { NavBar } from './component/NavBar';
+import Home from './component/Home';
+import AboutMe from './component/AboutMe';
+import Contact from './component/Contact';
+import Projects from './component/Projects';
+import Frelance from './component/Frelance';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [currentSection, setCurrentSection] = useState("home");
+  const [currentSection, setCurrentSection] = useState('home');
   useEffect(() => {
     // Función para actualizar currentSection basado en la posición de la página
     const handleScroll = () => {
-      const home = document.getElementById("home");
-      const about = document.getElementById("about-me");
-      const projects = document.getElementById("projects");
-      const freelance = document.getElementById("freelance");
-      const contact = document.getElementById("contact");
+      const home = document.getElementById('home');
+      const about = document.getElementById('about-me');
+      const projects = document.getElementById('projects');
+      const freelance = document.getElementById('freelance');
+      const contact = document.getElementById('contact');
       const scrollPosition = window.scrollY;
 
       if (
         scrollPosition >= home.offsetTop &&
         scrollPosition < about.offsetTop
       ) {
-        setCurrentSection("home");
+        setCurrentSection('home');
       } else if (
         scrollPosition >= about.offsetTop &&
         scrollPosition < projects.offsetTop
       ) {
-        setCurrentSection("about-me");
+        setCurrentSection('about-me');
       } else if (
         scrollPosition >= projects.offsetTop &&
         scrollPosition < freelance.offsetTop
       ) {
-        setCurrentSection("projects");
+        setCurrentSection('projects');
       } else if (
         scrollPosition >= freelance.offsetTop &&
         scrollPosition < contact.offsetTop
       ) {
-        setCurrentSection("freelance");
+        setCurrentSection('freelance');
       } else if (scrollPosition >= contact.offsetTop) {
-        setCurrentSection("contact");
+        setCurrentSection('contact');
       }
     };
 
     // Agregar un evento de escucha de desplazamiento
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
     // Eliminar el evento cuando el componente se desmonte
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []); // El segundo argumento [] asegura que esto solo se ejecute una vez al montar el componente
 
   return (
     <div className="App">
-      <NavBar
-        currentSection={currentSection}
-        setCurrentSection={setCurrentSection}
-      />
+      <NavBar currentSection={currentSection} />
       <Home />
       <AboutMe />
       <Projects />
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,74 +1,103 @@
-import React from "react";
-import "../styles/NavBar.scss";
-import { ReactComponent as UserIcon } from "../assets/icons/user-fill.svg";
-import { ReactComponent as PhoneIcon } from "../assets/icons/phone-fill.svg";
-import { ReactComponent as HomeIcon } from "../assets/icons/house-simple-fill.svg";
-import { ReactComponent as ProjectsIcon } from "../assets/icons/lightbulb-fill.svg";
-import { ReactComponent as ResumeIcon } from "../assets/icons/read-cv-logo-fill.svg";
-import { ReactComponent as MouseIcon } from "../assets/icons/mouse-simple-fill.svg";
+import React from 'react';
+import { useState } from 'react';
+import '../styles/NavBar.scss';
+import { ReactComponent as UserIcon } from '../assets/icons/user-fill.svg';
+import { ReactComponent as PhoneIcon } from '../assets/icons/phone-fill.svg';
+import { ReactComponent as HomeIcon } from '../assets/icons/house-simple-fill.svg';
+import { ReactComponent as ProjectsIcon } from '../assets/icons/lightbulb-fill.svg';
+import { ReactComponent as ResumeIcon } from '../assets/icons/read-cv-logo-fill.svg';
+import { ReactComponent as MouseIcon } from '../assets/icons/mouse-simple-fill.svg';
 
-export const NavBar = ({ currentSection, setCurrentSection }) => {
-  const handleClick = (sectionId) => {
+export const NavBar = ({ currentSection }) => {
+  const [NavBar, setNavBar] = useState(true);
+
+  const changeBackground = () => {
+    window.scrollY > 45 ? setNavBar(false) : setNavBar(true);
+  };
+
+  window.addEventListener('scroll', changeBackground);
+
+  const handleClick = sectionId => {
     // Scroll
     const section = document.getElementById(sectionId);
-    const yOffset = sectionId === "home" ? 0 : section.offsetTop;
+    const yOffset = sectionId === 'home' ? 0 : section.offsetTop;
     if (section) {
       window.scrollTo({
         top: yOffset,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     }
   };
 
   return (
-    <nav className="nav-bar">
+    <nav id="nav-bar" className={NavBar ? 'nav-bar' : 'nav-bar active'}>
       <button
-        onClick={() => handleClick("home")}
+        onClick={() => handleClick('home')}
         className={
-          currentSection === "home"
-            ? "nav-bar__icon icon-selected"
-            : "nav-bar__icon"
+          currentSection === 'home'
+            ? NavBar
+              ? 'nav-bar__icon active icon-selected'
+              : 'nav-bar__icon icon-selected'
+            : NavBar
+            ? 'nav-bar__icon active'
+            : 'nav-bar__icon '
         }
       >
         <HomeIcon />
       </button>
       <button
-        onClick={() => handleClick("about-me")}
+        onClick={() => handleClick('about-me')}
         className={
-          currentSection === "about-me"
-            ? "nav-bar__icon icon-selected"
-            : "nav-bar__icon"
+          currentSection === 'about-me'
+            ? NavBar
+              ? 'nav-bar__icon active icon-selected'
+              : 'nav-bar__icon icon-selected'
+            : NavBar
+            ? 'nav-bar__icon active'
+            : 'nav-bar__icon '
         }
       >
         <UserIcon />
       </button>
 
       <button
-        onClick={() => handleClick("projects")}
+        onClick={() => handleClick('projects')}
         className={
-          currentSection === "projects"
-            ? "nav-bar__icon icon-selected"
-            : "nav-bar__icon"
+          currentSection === 'projects'
+            ? NavBar
+              ? 'nav-bar__icon active icon-selected'
+              : 'nav-bar__icon icon-selected'
+            : NavBar
+            ? 'nav-bar__icon active'
+            : 'nav-bar__icon '
         }
       >
         <ProjectsIcon />
       </button>
       <button
-        onClick={() => handleClick("freelance")}
+        onClick={() => handleClick('freelance')}
         className={
-          currentSection === "freelance"
-            ? "nav-bar__icon icon-selected"
-            : "nav-bar__icon"
+          currentSection === 'freelance'
+            ? NavBar
+              ? 'nav-bar__icon active icon-selected'
+              : 'nav-bar__icon icon-selected'
+            : NavBar
+            ? 'nav-bar__icon active'
+            : 'nav-bar__icon '
         }
       >
         <MouseIcon />
       </button>
       <button
-        onClick={() => handleClick("contact")}
+        onClick={() => handleClick('contact')}
         className={
-          currentSection === "contact"
-            ? "nav-bar__icon icon-selected"
-            : "nav-bar__icon"
+          currentSection === 'contact'
+            ? NavBar
+              ? 'nav-bar__icon active icon-selected'
+              : 'nav-bar__icon icon-selected'
+            : NavBar
+            ? 'nav-bar__icon active'
+            : 'nav-bar__icon '
         }
       >
         <PhoneIcon />
